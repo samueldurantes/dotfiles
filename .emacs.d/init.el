ba274d85e5852355a5b267e112c3d1d1e10025f5
Samuel Durante <hyossha@gmail.com>
2021-06-24 15:56:46 -0300

    [emacs] reconfiguration

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,48 +1,37 @@
 (setq inhibit-startup-message t)
+(setq package-enable-at-startup nil)
 
+;; Appareance
 (tool-bar-mode 0)
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
 
-(global-linum-mode t)
-(global-set-key [f8] 'neotree-toggle)
-;; Font
-(set-face-attribute 'default nil :height 110)
+(load-theme 'vs-dark t)
 
-;; Theme
-(load-theme 'tango-dark)
+(set-face-attribute 'default nil :font "Ubuntu Mono" :height 160)
 
 ;; Packages
 (require 'package)
-
-(setq package-enable-at-startup nil)
-
-;; MELPA 
+(package-initialize)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/") t)
+(package-refresh-contents)
 
-(package-initialize)
+(require 'rust-mode)
 
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+;; Backups
+(setq backup-directory-alist `(("." . "~/.emacs.d/backups")))
 
-(use-package try
-  :ensure t)
-(use-package auto-complete
-  :ensure t
-  :init
-  (progn
-    (ac-config-default)
-    (global-auto-complete-mode t)))
+;; Keybinds
+(global-set-key (kbd "C-=") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
 
-;; MELPA Stuff
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(neotree auto-complete which-key try use-package)))
+ '(package-selected-packages '(vs-dark-theme ## rust-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
